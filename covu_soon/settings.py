947f55b67c3ebf272ad15c6daf72f012bd9f9b98
unknown <synergynigeria10@gmail.com>
2025-10-31 23:21:53 +0100

    added render.yaml and static server

--- covu_soon/settings.py
+++ covu_soon/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/coming-soon/"
 STATICFILES_DIRS = [
     BASE_DIR / "coming-soon",
