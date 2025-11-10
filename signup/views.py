c76b16452c6b03a6f8fe2fe5fd0d5b18d903c6c5
unknown <synergynigeria10@gmail.com>
2025-11-10 18:51:20 +0100

    Set pre-registered sellers count to 43 in frontend

--- signup/views.py
+++ signup/views.py
@@ -14,7 +14,6 @@ def coming_soon(request):
     """Render the coming soon landing page with pre-registered sellers count"""
     from .models import EmailSubscription
 
-    # Count all registered sellers (both verified and unverified)
     pre_registered_count = EmailSubscription.objects.count()
     return render(request, "main.html", {"pre_registered_count": pre_registered_count})
 
